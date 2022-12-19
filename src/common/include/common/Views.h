b1bf75d35a3ff22b49216f86052800b4c81d2435
araex <3930554+araex@users.noreply.github.com>
2022-12-19 20:33:19 +0100

    solve day 5

--- src/common/include/common/Views.h
+++ src/common/include/common/Views.h
@@ -18,4 +18,83 @@ namespace ax
     });
 
     inline constexpr auto stoi = ranges::views::transform([](const std::string &&s) { return std::stoi(s); });
+
+
+    // Flattens a range of ranges by iterating the inner
+    // ranges in round-robin fashion.
+    template<class Rngs>
+    class interleave_view : public ranges::view_facade<interleave_view<Rngs>>
+    {
+        friend ranges::range_access;
+        std::vector<ranges::range_value_t<Rngs>> rngs_;
+        struct cursor;
+        cursor begin_cursor()
+        {
+            return {0, &rngs_, ranges::views::transform(rngs_, ranges::begin) | ranges::to<std::vector>};
+        }
+
+      public:
+        interleave_view() = default;
+        explicit interleave_view(Rngs rngs)
+          : rngs_(std::move(rngs) | ranges::to<std::vector>)
+        {}
+    };
+
+    // From https://github.com/ericniebler/range-v3/blob/master/example/calendar.cpp#L192
+    // Flattens a range of ranges by iterating the inner
+    // ranges in round-robin fashion.
+    template<class Rngs>
+    struct interleave_view<Rngs>::cursor
+    {
+        std::size_t n_;
+        std::vector<ranges::range_value_t<Rngs>> *rngs_;
+        std::vector<ranges::iterator_t<ranges::range_value_t<Rngs>>> its_;
+        decltype(auto) read() const
+        {
+            return *its_[n_];
+        }
+        void next()
+        {
+            if(0 == ((++n_) %= its_.size()))
+                ranges::for_each(its_, [](auto &it) { ++it; });
+        }
+        bool equal(ranges::default_sentinel_t) const
+        {
+            if(n_ != 0)
+                return false;
+            auto ends = *rngs_ | ranges::views::transform(ranges::end);
+            return its_.end() != std::mismatch(its_.begin(), its_.end(), ends.begin(), std::not_equal_to<>{}).first;
+        }
+        CPP_member auto equal(cursor const &that) const
+            -> CPP_ret(bool)(requires ranges::forward_range<ranges::range_value_t<Rngs>>)
+        {
+            return n_ == that.n_ && its_ == that.its_;
+        }
+    };
+
+    // From https://github.com/ericniebler/range-v3/blob/master/example/calendar.cpp#L244
+    // In:  range<range<T>>
+    // Out: range<T>, flattened by walking the ranges
+    //                round-robin fashion.
+    auto interleave()
+    {
+        return ranges::make_view_closure([](auto &&rngs) {
+            using Rngs = decltype(rngs);
+            return interleave_view<ranges::views::all_t<Rngs>>(ranges::views::all(std::forward<Rngs>(rngs)));
+        });
+    }
+
+    // From https://github.com/ericniebler/range-v3/blob/master/example/calendar.cpp#L256
+    // In:  range<range<T>>
+    // Out: range<range<T>>, transposing the rows and columns.
+    auto transpose()
+    {
+        return ranges::make_view_closure([](auto &&rngs) {
+            using Rngs = decltype(rngs);
+            // CPP_assert(ranges::forward_range<Rngs>);
+            return std::forward<Rngs>(rngs) | interleave()
+                   | ranges::views::chunk(static_cast<std::size_t>(ranges::distance(rngs)));
+        });
+    }
+
 } // namespace ax
---
+++ src/day05/main.cpp
@@ -0,0 +1,185 @@
+#include <cctype>
+#include <charconv>
+#include <common/FileReader.h>
+#include <common/Views.h>
+#include <fmt/ranges.h>
+#include <map>
+#include <range/v3/all.hpp>
+#include <spdlog/spdlog.h>
+#include <stack>
+#include <tuple>
+
+namespace fs = std::filesystem;
+namespace rng = ranges;
+
+namespace
+{
+    bool isNumber(const std::string &s)
+    {
+        if(s.empty())
+        {
+            return false;
+        }
+        const size_t numberOfDigits
+            = rng::distance(s | rng::views::filter([](const char c) { return std::isdigit(c); }));
+        return numberOfDigits == s.size();
+    }
+
+    struct Instruction
+    {
+        int numberOfMoves = -1;
+        int from = -1;
+        int to = -1;
+    };
+
+    Instruction parseInstruction(std::string s)
+    {
+        auto numbers = s | rng::views::split(' ') | ax::to_string
+                       | rng::views::filter([](const std::string &s) { return isNumber(s); }) | ax::stoi
+                       | rng::to_vector;
+        assert(numbers.size() == 3);
+        return Instruction{numbers[0], numbers[1], numbers[2]};
+    }
+
+    void Part1()
+    {
+        const std::vector<std::string> lines = filereader::LineByLine(fs::path("./data/input.txt"));
+
+        auto stacks = lines
+                      | rng::views::split("")
+                      // part above the empty line contains the crate
+                      | rng::views::take_exactly(1) | rng::views::join
+                      | ax::transpose()
+                      // We only keep the columns starting with a digit, those are our stacks
+                      | rng::views::filter([](auto r) {
+                            const char c = *(std::next(r.begin(), rng::distance(r) - 1));
+                            return std::isdigit(c);
+                        })
+                      // remove whitespace
+                      | rng::views::transform(
+                          [](auto r) { return r | rng::views::filter([](const char c) { return c != ' '; }); })
+                      // at this point, the inner range is a forward_range, which makes it impossible to reverse.
+                      // we can fix that by evaluating the full range and convert to a vector. This gives us a
+                      // range<std::vector<char>>
+                      | rng::views::transform([](auto r) { return r | rng::to_vector; });
+
+        std::map<int, std::stack<char>> workspace;
+        for(auto rng : stacks)
+        {
+            const int idx = rng.back() - '0';
+            // std::stack will get a iterator constructor with c++23. So for now, do it manually
+            workspace[idx] = std::stack<char>();
+            for(const char c : rng | rng::views::reverse | rng::views::drop(1))
+            {
+                workspace[idx].push(c);
+            }
+        }
+
+        const std::vector<Instruction> instructions
+            = lines
+              | rng::views::split("")
+              // drop the crates, instructions are following
+              | rng::views::drop_exactly(1) | rng::views::take_exactly(1) | rng::views::join
+              | rng::views::transform([](const std::string &s) { return parseInstruction(s); }) | rng::to_vector;
+
+        for(const auto &instruction : instructions)
+        {
+            auto &from = workspace.at(instruction.from);
+            auto &to = workspace.at(instruction.to);
+            for(auto i = 0; i < instruction.numberOfMoves; ++i)
+            {
+                to.push(from.top());
+                from.pop();
+            }
+        }
+
+        spdlog::info("Part 1");
+        for(const auto &pair : workspace)
+        {
+            spdlog::info("{}: [{}]", pair.first, pair.second.top());
+        }
+    }
+
+    void Part2()
+    {
+        const std::vector<std::string> lines = filereader::LineByLine(fs::path("./data/input.txt"));
+
+        auto stacks = lines
+                      | rng::views::split("")
+                      // part above the empty line contains the crate
+                      | rng::views::take_exactly(1) | rng::views::join
+                      | ax::transpose()
+                      // We only keep the columns starting with a digit, those are our stacks
+                      | rng::views::filter([](auto r) {
+                            const char c = *(std::next(r.begin(), rng::distance(r) - 1));
+                            return std::isdigit(c);
+                        })
+                      // remove whitespace
+                      | rng::views::transform(
+                          [](auto r) { return r | rng::views::filter([](const char c) { return c != ' '; }); })
+                      // at this point, the inner range is a forward_range, which makes it impossible to reverse.
+                      // we can fix that by evaluating the full range and convert to a vector. This gives us a
+                      // range<std::vector<char>>
+                      | rng::views::transform([](auto r) { return r | rng::to_vector; });
+
+        std::map<int, std::stack<char>> workspace;
+        for(auto rng : stacks)
+        {
+            const int idx = rng.back() - '0';
+            // std::stack will get a iterator constructor with c++23. So for now, do it manually
+            workspace[idx] = std::stack<char>();
+            for(const char c : rng | rng::views::reverse | rng::views::drop(1))
+            {
+                workspace[idx].push(c);
+            }
+        }
+
+        const std::vector<Instruction> instructions
+            = lines
+              | rng::views::split("")
+              // drop the crates, instructions are following
+              | rng::views::drop_exactly(1) | rng::views::take_exactly(1) | rng::views::join
+              | rng::views::transform([](const std::string &s) { return parseInstruction(s); }) | rng::to_vector;
+
+        for(const auto &instruction : instructions)
+        {
+            auto &from = workspace.at(instruction.from);
+            auto &to = workspace.at(instruction.to);
+
+            // Gotta admit i'm a bit tired of fighting ranges::, just do Part2 quick & dirty.
+            std::stack<char> temp;
+            for(auto i = 0; i < instruction.numberOfMoves; ++i)
+            {
+                temp.push(from.top());
+                from.pop();
+            }
+            while(!temp.empty())
+            {
+                to.push(temp.top());
+                temp.pop();
+            }
+        }
+
+        spdlog::info("Part 2");
+        for(const auto &pair : workspace)
+        {
+            spdlog::info("{}: [{}]", pair.first, pair.second.top());
+        }
+    }
+} // namespace
+
+int main(int /*argc*/, const char ** /*argv*/)
+{
+
+
+    // const auto blub = stacks | rng::views::transform([](auto r) { return r | rng::views::reverse |
+    // rng::to_vector;
+    // });
+    // auto print = stacks
+    //              // prepare for print with fmt
+    //              | rng::views::transform([](auto r) { return r | rng::to_vector; }) | rng::to_vector;
+    // spdlog::info("{}", fmt::join(print, "\n"));
+    // spdlog::info("{}", print);
+    Part1();
+    Part2();
+}
